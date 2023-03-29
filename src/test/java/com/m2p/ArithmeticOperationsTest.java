1efd5dc97f8f9752e6d10585e5854121cde41df6
k-chandravir <kumar.chandravir@m2pfintech.com>
2023-03-29 12:44:55 +0530

    Add. Test Added For Substraction And The Subtract Function Added

--- src/test/java/com/m2p/ArithmeticOperationsTest.java
+++ src/test/java/com/m2p/ArithmeticOperationsTest.java
@@ -19,4 +19,25 @@ public class ArithmeticOperationsTest {
         //Assert
         assertEquals(expectedValue, actualValue);
     }
+
+    @Test
+    void toReturnSevenWhenThreeIsSubstractedFromTen(){
+        //Arrange
+        int expectedValue = 7;
+
+        //Act
+        int actualValue = arethmeticObject.subtract(10, 3);
+
+        //Assert
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    void toReturnMinusSevenWhenTenIsSubtractedFromThree(){
+        int expectedValue = -7;
+
+        int actualValue = arethmeticObject.subtract(3, 10);
+
+        assertEquals(expectedValue, actualValue);
+    }
 }
