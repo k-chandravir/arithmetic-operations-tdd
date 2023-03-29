fd3b3cffb4f1630c5c0f35d050484d665caf1d52
k-chandravir <kumar.chandravir@m2pfintech.com>
2023-03-29 12:35:52 +0530

    Add. test added for add operation and add operation added in Arithmetic Operation

--- src/test/java/com/m2p/ArithmeticOperationsTest.java
+++ src/test/java/com/m2p/ArithmeticOperationsTest.java
@@ -1,6 +1,22 @@
 package com.m2p;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ArithmeticOperationsTest {
 
     ArithmeticOperations arethmeticObject = new ArithmeticOperations ();
+
+    @Test
+    void toReturnThirteenWhenTenIsAddedToThree(){
+        //Arrange
+        int expectedValue = 13;
+
+        //Act
+        int actualValue = arethmeticObject.add(10, 3);
+
+        //Assert
+        assertEquals(expectedValue, actualValue);
+    }
 }
